5f7576bc7c25e93baebbbfbe5764335a76a7142f
icke1981 <ch.bastian1981@gmail.com>
2026-07-31 19:37:58 +0200

    Create chemistry.js

---
+++ chemistry.js
@@ -0,0 +1,24 @@
+// PoolPilot 2.4
+
+// Chemische Berechnungen
+
+function berechneAktivchlor(ph, dpd1, cya, temp) {
+
+    // Temperaturabhängiger pKs-Wert
+
+    const pKs = 7.54 - (0.028 * (temp - 25));
+
+    // Anteil Hypochlorsäure (HOCl)
+
+    const hocl = 1 / (1 + Math.pow(10, ph - pKs));
+
+    // Cyanursäure-Korrektur
+
+    const cyaFaktor = 1 / (1 + cya / 8);
+
+    // Aktiv wirksames Chlor
+
+    return dpd1 * hocl * cyaFaktor;
+
+}
+
